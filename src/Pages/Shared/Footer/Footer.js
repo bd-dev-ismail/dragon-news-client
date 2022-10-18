4f1be74fd074d12e61b99225b7c7d0c3e9fb1844
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-18 13:59:36 +0600

    folder structure ready

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <div>
+        <small>
+          <p>Copyright coming soon!</p>
+        </small>
+      </div>
+    );
+};
+
+export default Footer;
