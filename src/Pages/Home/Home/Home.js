02677c51093e0839ff23226a8d02695b0fea23fb
bd-dev-ismail <ismailratul7@gmail.com>
2022-10-20 09:52:41 +0600

    dynamically load data

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewsSummaryCard from '../../Shared/NewsSummaryCard/NewsSummaryCard';
 
 const Home = () => {
+    const allNews = useLoaderData();
     return (
         <div>
-            <h3>This is Home component</h3>
+            <h3>This is Home component {allNews.length}</h3>
+            {
+                allNews.map(news => <NewsSummaryCard
+                    key={news._id} news={news}
+                ></NewsSummaryCard>)
+            }
         </div>
     );
 };
